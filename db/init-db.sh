4ba2492e1f481eaa1c235097342204b7a167b440
Alexandre Debril <adebril@wolters-kluwer.fr>
2017-02-03 14:14:35 +0100

    make the script run inside the container

--- db/init-db.sh
+++ db/init-db.sh
@@ -7,11 +7,6 @@ $BASEDIR/create-db-statements.sh $1 > /var/data/sql/db-creation.sql
 cat $BASEDIR/mysql/$1-structure.sql >> /var/data/sql/db-creation.sql
 cat $BASEDIR/mysql/$1-data.sql >> /var/data/sql/db-creation.sql
 
-echo "#!/bin/bash" > /var/data/sql/init-db.sh
-echo 'mysql -u root -proot < /var/sql/db-creation.sql' >> /var/data/sql/init-db.sh
-
-chmod +x /var/data/sql/init-db.sh
-
-docker exec -it localdb /var/sql/init-db.sh
+mysql -u root -proot < /var/data/sql/db-creation.sql
 
 exit
