54ba4566c87127442234e200718efa2984c77aa6
Alex Debril <adebril@wolters-kluwer.fr>
2017-04-10 16:51:49 +0200

    bugfix

--- import-all.sh
+++ import-all.sh
@@ -6,5 +6,5 @@ for file in ${BASEDIR}/db/mysql/*-structure.sql; do
     filename=$(basename $file)
     dbname=`echo $filename | sed 's/-structure.sql//g'`
     echo "importing $dbname"
-    ${BASEDIR}/db/init-db.sh $dbname
+    ${BASEDIR}/bin/docker-mysql-import.sh $dbname
 done
