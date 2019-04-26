e4a4c10458441d2025677d6cd7fb10d8327944bf
ludovic claverie <ludovic.claverie-c@wolterskluwer.com>
2019-04-26 16:42:53 +0200

    export : exclude big tables data

--- bin/docker-mysql-export.sh
+++ bin/docker-mysql-export.sh
@@ -11,6 +11,21 @@ LOCAL_USER=root
 CONF_FILE="${BASEDIR}/../conf/mysql/${DATABASE}.conf"
 MYSQL_CONFIG=""
 
+EXCLUDED_TABLES=(
+logs
+_DEV_customers
+_DEV_customers_addresses
+_DEV_customers_tokens
+orders_baskets
+orders_details
+)
+
+IGNORED_TABLES_STRING=''
+for TABLE in "${EXCLUDED_TABLES[@]}"
+do :
+   IGNORED_TABLES_STRING+=" --ignore-table=${SOURCE_DB}.${TABLE}"
+done
+
 if [ -f "${CONF_FILE}" ]
 then
   echo "loading ${CONF_FILE}"
@@ -20,7 +35,7 @@ fi
 echo "Dumping $DATABASE structure"
 docker exec -it localdb mysqldump --compress --quick --skip-add-locks --single-transaction -h $HOST -u $HOST_USER -p$HOST_PASS $SOURCE_DB --no-data > ${TARGETDIR}/${DATABASE}-structure.sql
 echo "Dumping $DATABASE data"
-docker exec -it localdb mysqldump --compress --quick --skip-add-locks --single-transaction -h $HOST -u $HOST_USER -p$HOST_PASS $SOURCE_DB $MYSQL_CONFIG > ${TARGETDIR}/${DATABASE}-data.sql
+docker exec -it localdb mysqldump --compress --quick --skip-add-locks --single-transaction --no-create-info -h $HOST -u $HOST_USER -p$HOST_PASS $SOURCE_DB $MYSQL_CONFIG ${IGNORED_TABLES_STRING} > ${TARGETDIR}/${DATABASE}-data.sql
 
 exit
 
